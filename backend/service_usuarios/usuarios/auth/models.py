801566feb2435a54f7104b5cc6a72d13f29b98ae
juancruzCT <juancruzctclases12@gmail.com>
2025-05-13 18:54:36 -0300

    modelo de service_usuarios realizado

--- backend/service_usuarios/usuarios/auth/models.py
+++ backend/service_usuarios/usuarios/auth/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El usuario debe tener un correo electrónico')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, db_column='email_usuario')
+    nombre = models.CharField(max_length=255, db_column='nombre_usuario')
+    fecha_creacion = models.DateTimeField(default=timezone.now, db_column='fecha_creacion_usuario')
+    is_active = models.BooleanField(default=True, db_column='activo_usuario')
+    is_staff = models.BooleanField(default=False, db_column='admin_usuario')
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nombre']
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        db_table = 'usuarios'
