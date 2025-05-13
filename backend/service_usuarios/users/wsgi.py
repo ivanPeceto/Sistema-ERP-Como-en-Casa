f6eaad52e61fc77f73cbeef41ea5e6265da4330e
ivanleirbag <ivanpeceto20@gmail.com>
2025-05-13 19:31:22 -0300

    Corregidos problemas con la estructura del backend

--- backend/service_usuarios/users/wsgi.py
+++ backend/service_usuarios/users/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for usuarios project.
+WSGI config for users project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'usuarios.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'users.settings')
 
 application = get_wsgi_application()
