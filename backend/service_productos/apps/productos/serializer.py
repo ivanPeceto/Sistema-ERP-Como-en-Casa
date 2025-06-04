cd39cc68b47cb621409597f7e8e9edf773707169
ivanleirbag <169113267+ivanleirbag@users.noreply.github.com>
2025-06-03 21:21:47 -0300

    Integrada la tabla categorias en el microservicio de productos. Todos los serializers ajustados

--- backend/service_productos/apps/productos/serializer.py
+++ backend/service_productos/apps/productos/serializer.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Producto
+from apps.categorias.models import Categoria
 
 class ProductoSerializer(serializers.ModelSerializer):
+    categoria = serializers.SlugRelatedField(slug_field='nombre', queryset=Categoria.objects.all())
     class Meta:
         model = Producto
-        fields = ['id', 'nombre', 'descripcion', 'precio_unitario', 'precio_por_bulto', 'disponible']
+        fields = ['id', 'nombre', 'descripcion', 'precio_unitario', 'precio_por_bulto', 'disponible', 'categoria']
+
+
+    #def create(self, validated_data):
+            ##
