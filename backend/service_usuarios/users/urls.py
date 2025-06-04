028e17b3a5e159da28979b894c268b6e6df1aeb5
ivanleirbag <ivanpeceto20@gmail.com>
2025-06-04 12:17:46 -0300

    Cambios en los endpoints de usuarios

--- backend/service_usuarios/users/urls.py
+++ backend/service_usuarios/users/urls.py
@@ -2,7 +2,6 @@
 from django.contrib import admin
 from django.urls import path
 from apps.usuarios.views import UserLoginView, UserSignUpView
-from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
