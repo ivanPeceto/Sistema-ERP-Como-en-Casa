7d69624f84941ffee4c63b46d9dd2f66a3a3d5fd
ivanleirbag <ivanpeceto20@gmail.com>
2025-09-24 17:05:26 -0300

    fix: Establece zona horaria en Argentina

--- backend/service_pedidos/orders/settings.py
+++ backend/service_pedidos/orders/settings.py
@@ -123,11 +123,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_TZ = True
+
+USE_I18N = True
 
 
 # Static files (CSS, JavaScript, Images)
