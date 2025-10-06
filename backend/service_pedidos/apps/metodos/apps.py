09c6b1a1688d5dbe52c8864f76eda2942bbaf144
ivan_peceto <ivanpeceto20@gmail.com>
2025-10-05 21:16:52 -0300

    Revert "Add Multiple Payments and Unit Tests"

--- backend/service_pedidos/apps/metodos/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MetodosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.metodos'
--- backend/service_pedidos/apps/metodos/serializer.py
+++
@@ -1,9 +0,0 @@
-#/backend/service_pedidos/apps/metodos/serializer.py
-from rest_framework import serializers
-from .models import MetodoCobro
-
-class MetodoCobroSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = MetodoCobro
-        fields = ['id', 'nombre']
