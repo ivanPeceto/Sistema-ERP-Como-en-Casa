64e6f46a37b595776f989d19c60bc878ddb21fa8
jmrodriguezspinker <eionoman21@gmail.com>
2025-11-17 22:45:08 +0000

    feat(types): agregar nuevos tipos globales

---
+++ Frontend/src/types/types.ts
@@ -0,0 +1,18 @@
+// types.ts
+export const METODO_COBRO = {
+  efectivo: "Efectivo",
+  debito: "Débito",
+  credito: "Crédito",
+  mercadopago: "Mercado Pago",
+} as const;
+
+export const ESTADO_COBRO = {
+  activo: "Activo",
+  cancelado: "Cancelado"
+} as const;
+
+export type MetodoCobro = keyof typeof METODO_COBRO;
+
+export type EstadoCobro = keyof typeof ESTADO_COBRO;
+
+export type PedidoEstado = 'PENDIENTE' | 'LISTO' | 'ENTREGADO';
