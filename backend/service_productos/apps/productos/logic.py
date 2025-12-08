95e5e148031a879e5164ffb062cd6edf64332ff9
ivan <ivanpeceto20@gmail.com>
2025-12-08 19:08:08 -0300

    hotfix(back): Ajusta lógica de stock para evitar stock negativo

--- backend/service_productos/apps/productos/logic.py
+++ backend/service_productos/apps/productos/logic.py
@@ -9,7 +9,8 @@ def descontar_stock_recursivo_receta(receta, cantidad_consumida):
     for receta_insumo in receta.recetainsumo_set.all():
         insumo = receta_insumo.insumo
         cantidad_necesaria = receta_insumo.cantidad * cantidad_consumida
-        insumo.stock_actual -= cantidad_necesaria
+        nuevo_stock = insumo.stock_actual - cantidad_necesaria
+        insumo.stock_actual = max(Decimal('0.00'), nuevo_stock)
         insumo.save()
 
     # Descontar insumos en Sub-recetas (Recursión)
@@ -31,5 +32,6 @@ def procesar_venta_producto(producto, cantidad_vendida):
             descontar_stock_recursivo_receta(producto.receta, total_receta_a_consumir)
         else:
             if producto.stock is not None:
-                producto.stock -= int(cantidad_vendida)
+                nuevo_stock = producto.stock - int(cantidad_vendida)
+                producto.stock = max(0, nuevo_stock)
                 producto.save()
